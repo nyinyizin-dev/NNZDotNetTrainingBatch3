2b2c0366fa6a2eb52b6d30485382f695fef1973e
nyinyizin-dev <nyinyizin450@gmail.com>
2025-10-08 15:45:30 +0900

    feat: ado.net read

--- NNZDotNetTrainingBatch3.ConsoleApp/Program.cs
+++ NNZDotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string name = "ko ko";
+string fullName = (name == "ko ko") ? "Good day" : "Good Evening";
